a1491615fd4da32f1ca8040f2e52743fb6f34049
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2023-02-19 11:18:44 +0100

    Initial commit

---
+++ src/main/kotlin/de/hglabor/snorlaxboss/SnorlaxBoss.kt
@@ -0,0 +1,10 @@
+package de.hglabor.snorlaxboss
+
+import net.fabricmc.api.ModInitializer
+
+class SnorlaxBoss : ModInitializer {
+
+    override fun onInitialize() {
+        //TODO - "Not yet implemented"
+    }
+}
